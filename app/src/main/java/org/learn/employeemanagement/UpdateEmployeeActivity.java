145d990ab324170adc13925145f7cd8b3fe197a4
prashmittanay <prashmittanay@gmail.com>
2020-09-25 12:30:37 +0530

    Employee Update code added

--- app/src/main/java/org/learn/employeemanagement/UpdateEmployeeActivity.java
+++ app/src/main/java/org/learn/employeemanagement/UpdateEmployeeActivity.java
@@ -2,10 +2,16 @@ package org.learn.employeemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class UpdateEmployeeActivity extends AppCompatActivity {
+    private static final String TAG = "UpdateEmployeeActivity";
     private Employee mEmployee;
     private EditText mId;
     private EditText mName;
@@ -18,7 +24,7 @@ public class UpdateEmployeeActivity extends AppCompatActivity {
         mEmployee = (Employee) getIntent().getSerializableExtra("Employee");
         mId = findViewById(R.id.edittext_id);
         mName = findViewById(R.id.edittext_name);
-        mDesignation = findViewById(R.id.edittext_designation);
+        mDesignation = findViewById(R.id.edittext_department);
 
         mId.setText(mEmployee.getId());
         mId.setEnabled(false);
@@ -26,5 +32,34 @@ public class UpdateEmployeeActivity extends AppCompatActivity {
         mDesignation.setText(mEmployee.getDepartment());
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        Button update = findViewById(R.id.button_employee_update);
+        update.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                updateEmployee();
+            }
+        });
+
+
+
+    }
+
+    private void updateEmployee() {
+        ContentValues contentValues = new ContentValues();
+        String name = ((EditText)findViewById(R.id.edittext_name)).getText().toString();
+        String department = ((EditText)findViewById(R.id.edittext_department)).getText().toString();
+        contentValues.put(EmployeeContentProvider.NAME, name);
+        contentValues.put(EmployeeContentProvider.DEPARTMENT, department);
+
+        String selectionClause = "_ID = ?";
+        String[] selectionArgs = {mEmployee.getId()};
+
+        int count = getContentResolver().update(EmployeeContentProvider.CONTENT_URI, contentValues, selectionClause, selectionArgs);
+
+        Log.d(TAG, String.valueOf(count));
+    }
 }
