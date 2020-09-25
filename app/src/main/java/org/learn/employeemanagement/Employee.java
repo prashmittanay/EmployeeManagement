7d019f9971f400caf21a2cd6e63fd23ebee128d0
prashmittanay <prashmittanay@gmail.com>
2020-09-25 09:12:34 +0530

    Employee Class added

---
+++ app/src/main/java/org/learn/employeemanagement/Employee.java
@@ -0,0 +1,50 @@
+package org.learn.employeemanagement;
+
+public class Employee {
+    private String id;
+    private String name;
+    private String department;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(String id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+}
