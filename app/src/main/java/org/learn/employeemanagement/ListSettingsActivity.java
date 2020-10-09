8c48fb4eba35df7a48b80d1adeed12fc3debf012
Prashmit Tanay <prashmittanay@gmail.com>
2020-10-09 09:51:36 +0530

    layout added

---
+++ app/src/main/java/org/learn/employeemanagement/ListSettingsActivity.java
@@ -0,0 +1,14 @@
+package org.learn.employeemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_settings);
+    }
+}
