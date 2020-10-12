f638f3acb1e249520f72a6c2fa3c2a4b84ce5c46
Prashmit Tanay <prashmittanay@gmail.com>
2020-10-12 09:13:03 +0530

    expanded view added

--- app/src/main/java/org/learn/employeemanagement/CustomCursorAdapter.java
+++ app/src/main/java/org/learn/employeemanagement/CustomCursorAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CustomCursorAdapter extends CursorAdapter {
@@ -18,21 +19,24 @@ public class CustomCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.listview_row_layout, parent, false);
+        return LayoutInflater.from(context).inflate(R.layout.listview_expanded_row_layout, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView employeeIdTextView = (TextView) view.findViewById(R.id.text_employeeID);
-        TextView employeeNameTextView = (TextView) view.findViewById(R.id.text_employeeName);
-        TextView employeeDepartmentTextView = (TextView) view.findViewById(R.id.text_employeeDepartment);
+        TextView employeeIdTextView = (TextView) view.findViewById(R.id.expanded_text_employeeID);
+        TextView employeeNameTextView = (TextView) view.findViewById(R.id.expanded_text_employeeName);
+        TextView employeeDepartmentTextView = (TextView) view.findViewById(R.id.expanded_text_employeeDepartment);
+        ImageView employeePictureImageView = (ImageView) view.findViewById(R.id.imageview_list_picture_preview);
 
         int id = cursor.getInt(cursor.getColumnIndex(EmployeeContentProvider._ID));
         String name = cursor.getString(cursor.getColumnIndex(EmployeeContentProvider.NAME));
         String department = cursor.getString(cursor.getColumnIndex(EmployeeContentProvider.DEPARTMENT));
+        String picture = cursor.getString(cursor.getColumnIndex(EmployeeContentProvider.PICTURE));
 
         employeeIdTextView.setText(String.valueOf(id));
         employeeNameTextView.setText(name);
         employeeDepartmentTextView.setText(department);
+        CameraUtils.drawImage(picture, employeePictureImageView);
     }
 }
